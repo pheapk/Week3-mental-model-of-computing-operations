6e24aa8af4c725907a97c51cc5beef0f939861dd
Pheap <khmoping@gmail.com>
2021-10-09 21:21:57 -0700

    pass and fire function

---
+++ pass-fire-function.js
@@ -0,0 +1,12 @@
+// Passing and Call function
+
+var red = function red () {
+  return 3.141;
+}
+
+const blue = function blue(f){ // 
+  let y = f(); // call function red
+  return y; return value return from function red
+}
+
+var x = blue(red); //passing red fuction as reference
