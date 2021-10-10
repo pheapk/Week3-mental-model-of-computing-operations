b87025bfbcf41bfd090355d417d21c93f93cc3eb
Pheap <khmoping@gmail.com>
2021-10-09 19:50:39 -0700

    scope of variables

---
+++ scope-of-variables.js
@@ -0,0 +1,23 @@
+function blue(x) {
+    var y = 0;
+    let l = 3;
+    const c = 5;
+    if (y<=0) {
+      let x = y;
+      var x2 = y +1;
+      const x3= y + 3;
+      const c = 10;
+      x = x +1;
+      y = z;
+      y = j;
+      l = x;
+    }
+    y = y+1;
+    return y+1;
+  }
+  
+  var x = 1;
+  let z = 4
+  const j = 8;
+  console.log('result = ' + blue(x));
+  //console.log('x2 = ' + x2);
